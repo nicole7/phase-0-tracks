97003c3d8fb07849fb5dc7df9ae9e30358c18ca4
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-06-23 20:31:34 -0500

    Went back to correct

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,18 @@
+puts "How many employees are there?"
+emp_total = gets.to_i
 i = 1
 
-while i <= 4
+
+while i <= emp_total
 puts "What's the employee's name"
 name = gets.chomp
-puts "What are your allergies? Type 'done' when finished"
+puts "What are your allergies?"
 allergies = gets.chomp
-  if allergies == "done" 
-    puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
-  i += 1
+
+  if allergies == "sunshine" 
+    puts "Probably a vampire"
   end
+i += 1
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
