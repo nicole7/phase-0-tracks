e5d357021c6a7a0becddf05f599f1db8b6925c67
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-06-23 23:15:06 -0500

    Complete

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,14 +1,18 @@
 puts "What is the hamster's name?"
-name=gets.chomp
+name_hamster=gets.chomp
 puts "From 1 - 10 how loud are they?"
-volume=gets.chomp
+volume=gets.to_i
 puts "What is their fur color?"
 fur=gets.chomp
 puts "Are they a good canditate for adoption?"
 adoptable=gets.chomp
 puts "How old are they?"
-age=gets.chomp
-if age == ""
+age=gets.to_i
+if age == nil
 	age = nil
 end
-puts age
+
+puts "The name is #{name_hamster}. The volume level of the hamster is
+#{volume} and their fur color #{fur}. #{adoptable} to adoption. They are
+#{age} years old"
+p
