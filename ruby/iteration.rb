d1d7028249436a48ee02bf07fe05786e5fce69e8
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-06-29 22:34:47 -0500

    Complete 5.3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -33,5 +33,30 @@ puts "These are only animals"
 p hash_list.each  { |i, j| j.upcase!}
 	
 
+list = [1,2,3,4,5,6,7,8,9,10]
+
+def list_method
+list.delete_if{|list| list < 5} 
+list.keep_if{|list| list < 5} 
+list.select { |list| list.even?}
+list.drop_while { |list| list < 5}
+end
+
+list_method
+
+
+hash_list ={ 1 => 'apples', 2 => 'bananas', 3 => 'cat'}
+
+def hash_method
+hash_list.delete_if{|key, value| key == 1}
+hash_list.keep_if{|key, value| key == 1}
+hash_list.select{|key, value| value.include? 'p'}
+hash_list.drop_while { |key, value| value.include? 's'}
+end
+
+hash_method
+	
+
+
 
 
