15929a895d7c3eb82a69c98bab86b64a1801f266
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-02 11:10:35 -0500

    Update 5.3 with comments

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,62 +1,96 @@
-array = [3,3,6,5]
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# using .each
+
+puts "Before:"
 array.each do |array|
-	puts array
+    puts array
 end
 
-puts "divide"
+puts "After"
+array.each do |array|
+    puts 2*array
+end
 
-array.map {|i| puts 2*i}
+# using .map
 
-puts "divide"
-
+puts "Before:"
+array.map {|i| i}
 p array
 
+puts "After"
+array.map! {|i| 2*i}
+p array
 
-hash_list = {
-	'bird' => 'Swallow',
-	'mammal' => 'Rhino',
-	'amphibian' => 'Frog',
-	'insect' => 'Stag Beatle'
+#############################
+
+hash = {
+    'bird' => 'swallow',
+    'mammal' => 'rhino',
+    'amphibian' => 'frog',
+    'insect' => 'stag beetle'
 }
 
-puts "These are some creatures"
+# using .each
 
-hash_list.each do |animal, example|
-	puts "A #{example} is an example of a #{animal}"
+puts "Some creatures:"
+hash.each do |i,j|
+	puts "One example of a #{i} is a #{j}"
 end
 
-
-puts "These are only animals"
-
-
-p hash_list.each  { |i, j| j.upcase!}
-	
-
-list = [1,2,3,4,5,6,7,8,9,10]
-
-def list_method
-list.delete_if{|list| list < 5} 
-list.keep_if{|list| list < 5} 
-list.select { |list| list.even?}
-list.drop_while { |list| list < 5}
+puts "Only animals:"
+hash.each do |i,j|
+	puts "One example of a #{i} is a #{j}"
 end
 
-list_method
-
-
-hash_list ={ 1 => 'apples', 2 => 'bananas', 3 => 'cat'}
-
-def hash_method
-hash_list.delete_if{|key, value| key == 1}
-hash_list.keep_if{|key, value| key == 1}
-hash_list.select{|key, value| value.include? 'p'}
-hash_list.drop_while { |key, value| value.include? 's'}
+puts "More excitement:"
+hash.each do |i,j| j.upcase!
+	puts "One example of a #{i} is a #{j}"
 end
 
-hash_method
-	
+#############################
 
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p array
 
+# iterate thru, delete matches
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p array.delete_if {|i| i < 5} 
+
+# filter thru, keep matches
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p array.keep_if {|i| i < 5} 
+
+# filter thru, keep matches
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p array.select {|i| i.even?}
+
+# remove items until false
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p array.drop_while {|i| i < 5}
+
+#############################
+
+hash = {1 => 'apples', 2 => 'bananas', 3 => 'cat'}
+puts hash
+
+# iterate thru, delete matches
+hash = {1 => 'apples', 2 => 'bananas', 3 => 'cat'}
+hash.delete_if {|i, j| i == 1}
+puts hash
+
+# filter thru, keep matches
+hash = {1 => 'apples', 2 => 'bananas', 3 => 'cat'}
+hash.keep_if {|i, j| i == 1}
+puts hash
+
+# filter thru, keep matches
+hash = {1 => 'apples', 2 => 'bananas', 3 => 'cat'}
+hash.select! {|i, j| j.include? 'p'}
+puts hash
+
+# remove items until false
+hash = {1 => 'apples', 2 => 'bananas', 3 => 'cat'}
+p hash.drop_while {|i, j| j.include? 's'}
 
 
