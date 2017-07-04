04a4f4eaf9fe70f2e1de496c16773c40e0fe57d3
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-04 15:10:02 -0500

    Completed, yay

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,57 +7,83 @@
 
 
 
-#Create method that will get information
+#Create method that will fill a hash with answers to a key and then change a value
 def designer_method
-	#Empty hash
+	#Create an empty hash
 	designer_hash = {}
 		
-	#Ask questions for user while addin input to hash
+	#Ask the user for their name
 		puts "What is your name?"
+		#Store their answer in the variable name
 		name = gets.chomp
-		designer_hash[:name] = name
+		#Take name and put as value inside (and create) the key Name in the hash
+		designer_hash["Name"] = name
+		#Ask for name
 		puts "What is your age?"
+		#Put input in age - change to explicit take of integer
 		age = gets.to_i
-		designer_hash[:age] = age
+		#Take input and put as value in another created key called Age
+		designer_hash["Age"] = age
+		#Ask user how many children they have 
 		puts "How many children do you have?"
+		#Get integer input
 		num_child = gets.to_i
-		designer_hash[:num_child] = num_child
+		#Store input in the value of the key Number of Children of hash
+		designer_hash["Number of Children"] = num_child
+		#Ask user their favorite decor theme
 		puts "What is your favroite decor theme?"
+		#Store in variable that now takes explicit string 
 		decor_theme = gets.chomp
-		designer_hash[:decor_theme] = decor_theme
-		puts "Have you been doing this a long time? (Yes or No)"
+		#Store input in value for key Decor theme
+		designer_hash["Decor Theme"] = decor_theme
+		#Ask user yes or no answer
+		puts "Have you been doing this a long time? ('Yes' or 'No')"
+		#Take in input if completely correct and
 		time = gets.chomp
+			#Test if "Yes"
 			if time == "Yes"
+				#make it equal to true
 				time = true
+			#Otherwise
 			else
+				#It will equal false (because most likely no)
 				time = false
+			#End if statement
 			end
-			designer_hash[:time] = time
-			
-	return designer_hash
-	
+			#Add time value true or false to the key, Time
+			designer_hash["Time"] = time
+	#Return the newly filled hash		
+	 designer_hash
+#End method	
 end
 
-
-
-#Create a method that will ask if they want to update revious input
-def designer_update(hash)
+#Create a method that will ask if they want to update previous input
+def designer_update(hash_name)
 	#Ask the user which category they want to modify and with what new answer
 	puts "Would you like update one of these categories? (Enter 'none' if no changes"
+	#Get input 
 	input = gets.chomp
+		#If they type none then 
 		if input == 'none'
+			#just print the hash as is
 			p designer_hash
+		#Otherwise if input was a key, ask what they would like to updated the value to
 		else puts "Please enter your new answer"
+			#Take their input
 			 update = gets.chomp
 
-			 #Enter the key they chose and the new value into the hash
-			designer_hash[input] = update
+			 #Enter the key they chose within the hash and have the key now equal their updated value
+			hash_name[input] = update
+		#End if statement
 		end
+		#Print this hash whether updated or not
+		p hash_name
+#End method
 end
 
-#Call method to add user input to hash
-designer_method
-#Print updated hash
-p designer_hash
-#Call method to change hash if user needs to
-designer_update(designer_hash)
+#Set a variable to store the method that created and returned the hash
+get_hash = designer_method
+#Call variable to print the hash created in this method
+p get_hash
+#Call method to ask user if they want to change hash by passing the new hash variable as an argument through it
+designer_update(get_hash)
