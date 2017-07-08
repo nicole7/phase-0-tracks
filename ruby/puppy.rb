0f95b448e409683fca0f706d35b52db8dcddca79
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-08 10:21:03 -0500

    Complete

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+Species
+Boopedus Snootus
+
+Characteristics
+Snout Length: Varies
+Weight: Varies
+Size: Small
+Name: Varies
+Cute: Yes
+Leg Count: 4
+Potty Training Status: Intermediate 
+
+Behavior
+Run
+Bark
+Eat
+Sleep
+Play
+
+
+
