beee2cc7b30ea00441de52318b48c66f1a5d0cf4
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-10 22:01:16 -0500

    Completed missing code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,37 @@
+p "What is your name?"
+name = gets.chomp
+
+p "How old are you?"
+age = gets.chomp.to_i
+
+p "Do you like garlic bread? (Yes or No)"
+eat_garlic_bread = gets.chomp
+	if eat_garlic_bread == "Yes"
+	   eat_garlic_bread = True
+	else eat_garlic_bread = False
+	end
+
+p "Would you like to enroll in our insurance program? (Yes or No)"
+insurance = gets.chomp
+	if insurance == "Yes"
+		insurance = True
+	else insurance = False
+	end
+
+if age < 100 && eat_garlic_bread 
+	p"Probably not a vampire"
+elsif age >= 100 && !(eat_garlic_bread || insurance)
+	p "Probably a vampire"
+elsif age >= 100 & !(eat_garlic_bread && insurance)
+	p "Almost certainly a vampire"
+elsif name == "Drake Cula" || "Tu Fang"
+	p "Definitly a vampire"
+else
+	"Result inconclusive"
+end
+
+
+
 puts "How many employees are there?"
 emp_total = gets.to_i
 i = 1
