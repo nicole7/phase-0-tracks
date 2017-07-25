4f4fbb98e063f34a2d963706afa654759e4fc6fa
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-24 21:27:38 -0500

    done

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'list'
+require_relative 'todoList'
 
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
@@ -9,15 +9,15 @@ describe TodoList do
 
   it "adds an item to the list" do
     list.add_item("mop")
-    expect(list.get_items["do the dishes", "mow the lawn"]).to eq ["do the dishes", "mow the lawn", "mop"]
+    expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
   end
 
   it "deletes an item" do
     list.delete_item("do the dishes")
-    expect(list.get_items["do the dishes", "mow the lawn"]).to eq ["mow the lawn"]
+    expect(list.get_items).to eq ["mow the lawn"]
   end
 
   it "retrieves an item by index" do
-    expect(list.get_item[0]).to eq "do the dishes"
+    expect(list.get_item).to eq "do the dishes"
   end
 end
